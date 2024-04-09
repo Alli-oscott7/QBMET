c48233c3db5911b0182b4eb3e96040753f38ac5b
Alli-oscott7 <124524581+Alli-oscott7@users.noreply.github.com>
2024-04-09 12:54:32 -0400

    Writing to csv file properly now

--- EEGThreshold.py
+++ EEGThreshold.py
@@ -1,8 +1,6 @@
-import neurol
 from neurol import streams
 from pylsl import StreamInlet, resolve_stream
 import numpy as np
-from pylsl import StreamInlet, resolve_stream
 from scipy.signal import butter, filtfilt
 
 import csv
@@ -22,38 +20,89 @@ def highpass_filter(data, cutoff, fs, order=5):
 def getEEGValues():
     streams1 = resolve_stream("name='Unicorn'")
     inlet = StreamInlet(streams1[0])
-    stream = streams.lsl_stream(inlet,buffer_length=1024)
+    stream = streams.lsl_stream(inlet, buffer_length=1)
 
     if not stream:
-        print("There is no values being read through the EEG Stream!")
+        print("There are no values being read through the EEG Stream!")
         return -1
-
-    # Data is returned in the format:
-    # [EEG1, EEG2, ..., EEG8, Accel X, Accel Y, Accel Z, Gyro X, Gyro Y, Battery %, Counter, Indicator (On/Off, always 1)
-    #This sample[0] means that we are pulling only the data from the first node
-    data = []
+        
+    # This sample[0] means that we are pulling only the data from the first node
+    raw_data = []
     for _ in range(20):
         sample, timestamp = inlet.pull_sample()
         ext_sample = sample[0]
-        #print(timestamp, sample)
-        data.append(sample[0])
+        print(timestamp, sample)
+        raw_data.append(sample[0])
 
-    #print(data)
-    filtered_data = highpass_filter(data, cutoff=60, fs=250)
-    #print(filtered_data)
-    return (sum(filtered_data) / len(filtered_data))
+    filtered_data = highpass_filter(raw_data, cutoff=60, fs=250)
+    return raw_data, filtered_data
+
+
+
+
+def getEEGValuesInCSV(num_data_points):
+    streams1 = resolve_stream("name='Unicorn'")
+    inlet = StreamInlet(streams1[0])
+    stream = streams.lsl_stream(inlet, buffer_length=1)
+
+    if not stream:
+        print("There are no values being read through the EEG Stream!")
+        return -1
+        
+    # This sample[0] means that we are pulling only the data from the first node
+    raw_data = []
+    
+
+    sample_number = 20
+    averaged_data_list = []
+    averaged_filtered_data_list = []
+
+    for k in range(num_data_points):
+        data_totals = []
+        filtered_data_totals = []
+        for i in range (17):
+            data_totals.append(0)
+            filtered_data_totals.append(0)
+
+        for _ in range(sample_number):
+            sample, timestamp = inlet.pull_sample()
+            #print(timestamp, sample)
+            #filtered_sample = highpass_filter(sample, cutoff=60, fs=250)
+            filtered_sample = sample
+            #print(sample)
+            #print("Filtered sample: ")
+            #print(filtered_sample)
+            for i in range (len(sample)):
+                data_totals[i] += sample[i]
+                filtered_data_totals[i] += filtered_sample[i]
+        
+        #print(averaged_filtered_data_list)
+        # Average the totals
+        average_data = [x / sample_number for x in data_totals]
+        filtered_average_data = [x / sample_number for x in filtered_data_totals]
+        averaged_data_list.append(average_data)
+        averaged_filtered_data_list.append(filtered_average_data)
+    
+        
+
+    with open('averaged_eeg_data.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        # Write header row
+        writer.writerow(['EEG1', 'EEG2', 'EEG3', 'EEG4', 'EEG5', 'EEG6', 'EEG7', 'EEG8',
+                         'Accel X', 'Accel Y', 'Accel Z', 'Gyro X', 'Gyro Y',
+                         'Battery %', 'Counter', 'Indicator'])
+
+        # Record start time
+        for i in range(num_data_points):
+            writer.writerow(averaged_filtered_data_list[i])
+
 
 def EEGThreshold(threshold):
-    EEG_val = getEEGValues()
-    print(EEG_val)
-    if (EEG_val > threshold):
-        print("Greater than the threshold!")
-        return 1
-    else:
-        print("Less than the threshold!")
-        return 0
-#change
-
+    raw_data, filtered_data = getEEGValues()
+    print("Raw EEG Data:", filtered_data)
+    print("Filtered EEG Data:", filtered_data)
+   # return (sum(filtered_data) / len(filtered_data))
 
 def average_and_output_csv():
     # Open CSV file for writing
@@ -68,9 +117,12 @@ def average_and_output_csv():
         # Record start time
         start_time = time.time()
 
-        while time.time() - start_time < 10:
+        while time.time() - start_time < 5:
             # Get EEG data
-            eeg_data = getEEGValues()
+            unfiltered_eeg_data, eeg_data = getEEGValues()
+            #eeg_data = eeg_data.tolist()
+
+            # print("EEG_DATA", eeg_data)
 
             # Separate data into EEG and sensor data
             eeg_values = eeg_data[:8]
@@ -80,15 +132,44 @@ def average_and_output_csv():
             counter = eeg_data[14]
             indicator = eeg_data[15]
 
+            #print("Indicator is: " + str(eeg_data))
+
+            # print("Indicator", eeg_data[15])
+            chunk_size = 16
+            length = len(eeg_data)
+            start_index = 0
+            end_index = min(chunk_size, length)
+            while start_index < length:
+                chunk = eeg_data[start_index:end_index]
+                if (len(chunk) != 0):
+                    print("Chunk", chunk)
+
+                start_index = end_index
+                end_index = min(start_index + chunk_size, length)
+
+                writer.writerow(chunk)
+
             # Average sensor data
-            averaged_sensor_data = [sum(sensor_data) / len(sensor_data) for _ in range(len(sensor_data))]
+            #averaged_eeg_values = [sum(eeg_values) / len(eeg_values) for _ in range(len(eeg_values))]
+            #averaged_sensor_data = [sum(sensor_data) / len(sensor_data) for _ in range(len(sensor_data))]
+            #averaged_gyro_data = [sum(gyro_data) / len(gyro_data) for _ in range(len(gyro_data))]
 
             # Write averaged data to CSV file
-            writer.writerow(eeg_values + averaged_sensor_data + gyro_data + [battery_percentage, counter, indicator])
+            # writer.writerow(eeg_data)
+            #for i in range(len(eeg_data)):
+            #    writer.writerow(eeg_values[i] + sensor_data[i] + gyro_data[i] + [battery_percentage, counter, indicator])
 
             # Wait for a short time before next iteration
             time.sleep(0.1)  # Adjust as needed
-
+    print("Indicator is: " + str(unfiltered_eeg_data))
 
 print("In main")
-EEGThreshold(0.1)
+#EEGThreshold(0.1)
+#average_and_output_csv()
+start_time = time.time()
+print("Starting time = " + str(start_time))
+getEEGValuesInCSV(735)
+end_time = time.time()
+print("Ending time = " + str(end_time))
+print("total elapsed time = " + str(end_time - start_time))
+
